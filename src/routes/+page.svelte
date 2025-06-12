24df7ec06a2198843668a2d14b44de245cf3efaf
Maksiks <maksiksking@gmail.com>
2025-06-12 18:23:00 +0300

    feat: re-added basic page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Lightbox from "$lib/Lightbox.svelte";
+    import Settings from "$lib/Settings.svelte";
+    import Dockbar from "$lib/Dockbar.svelte";
+
+    const time:Date = new Date();
+
+    let welcome = $state('JUST CARRY ON');
+</script>
+
+<Lightbox is-lightbox=showLightbox />
+<Settings/>
+<main class="main-wrapper">
+    <div>
+        <div class="h-text">
+            <h1>{{ welcome }}</h1>
+        </div>
+        <div class="pic-cover"></div>
+        <Dockbar/>
+        <img src="@/assets/pic.png" alt="beans"/>
+        <div class="time">
+            <h2>{ time.toLocaleTimeString("de-De").slice(0, 5) }</h2>
+        </div>
+    </div>
+    <!--    <Searchbar/>-->
+</main>
