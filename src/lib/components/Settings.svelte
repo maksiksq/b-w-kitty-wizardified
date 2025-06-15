10238e161c6a06ea28b0e8a42368db3fff36503a
Maksiks <maksiksking@gmail.com>
2025-06-15 14:35:02 +0300

    fix: minor optimization

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -1,32 +1,31 @@
 <script lang="ts">
-import ColorPicker from 'svelte-awesome-color-picker';
-import Colorpick from '$lib/components/Colorpick.svelte';
+	import ColorPicker from 'svelte-awesome-color-picker';
+	import Colorpick from '$lib/components/Colorpick.svelte';
 
-const { defaultColors } = $props();
+	const { defaultColors } = $props();
 </script>
 
 <div class="settings-wrap">
 	<div class="settings-wrap-inner">
-		<Colorpick {defaultColors} value="bg-color"/>
-		<Colorpick {defaultColors} value="text-color"/>
-		<Colorpick {defaultColors} value="accent-color"/>
-		<Colorpick {defaultColors} value="accent-color2"/>
+		{#each Object.keys(defaultColors) as value}
+			<Colorpick {defaultColors} {value} />
+		{/each}
 	</div>
 </div>
 
 
 <style>
-		.settings-wrap {
+    .settings-wrap {
         position: absolute;
-				top: 0;
-				left: 0;
+        top: 0;
+        left: 0;
 
-				pointer-events: none;
+        pointer-events: none;
 
-				width: 100vw;
-				height: 100vh;
+        width: 100vw;
+        height: 100vh;
 
-				z-index: 10000;
+        z-index: 10000;
 
         display: flex;
         flex-direction: row;
@@ -37,17 +36,17 @@ const { defaultColors } = $props();
             width: 50vw;
             height: 50vh;
 
-						user-select: none;
+            user-select: none;
 
             pointer-events: auto;
 
-						/* inner color picker bits */
+            /* inner color picker bits */
             --cp-bg-color: var(--bg-color);
             --cp-border-color: var(--accent-color2);
             --cp-text-color: white;
             --cp-input-color: var(--accent-color);
             --cp-button-hover-color: var(--accent-color);
         }
-		}
+    }
 
 </style>
