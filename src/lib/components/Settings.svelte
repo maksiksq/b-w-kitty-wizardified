25ac33117e9da91d28c8f0ea72db070d60311840
Maksiks <maksiksking@gmail.com>
2025-06-15 19:34:33 +0300

    fix: added editor button

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import ColorPicker from 'svelte-awesome-color-picker';
 	import Colorpick from '$lib/components/Colorpick.svelte';
+	import Fa from 'svelte-fa';
+	import { fas } from '@fortawesome/free-solid-svg-icons';
 
 	let {
 		defaultColors,
@@ -15,18 +17,25 @@
 
 <div class="settings-wrap">
 	<div class="settings-wrap-inner">
-		{#each Object.keys(defaultColors) as value}
-			<Colorpick {defaultColors} {value} bind:isDarkColor />
-		{/each}
-		<div class="tint-bloc settings-bloc">
-			<input name="tint" type="checkbox" bind:checked={tempOverlay} />
-			<label for="tint">Image tint</label>
-		</div>
-		<div class="welcome-bloc settings-bloc">
-			<input name="welcome" type="text" bind:value={settings.welcome}
-						 onkeydown={(e) => {e.key === 'Enter' ? showLightbox=false : ''}} />
-			<label for="welcome">Upper text</label>
-		</div>
+		<section class="settings-bloc-cont">
+			{#each Object.keys(defaultColors) as value}
+				<Colorpick {defaultColors} {value} bind:isDarkColor />
+			{/each}
+			<div class="tint-bloc settings-bloc">
+				<input name="tint" type="checkbox" bind:checked={tempOverlay} />
+				<label for="tint">Image tint</label>
+			</div>
+			<div class="welcome-bloc settings-bloc">
+				<label for="welcome">Upper text: &nbsp</label>
+				<input name="welcome" type="text" bind:value={settings.welcome}
+							 onkeydown={(e) => {e.key === 'Enter' ? showLightbox=false : ''}} />
+			</div>
+		</section>
+		<section class="editor-bloc-cont">
+			<button class="editor-button">
+				<Fa icon={fas['faPenToSquare']}></Fa>
+			</button>
+		</section>
 
 	</div>
 </div>
@@ -55,19 +64,58 @@
             height: 50vh;
 
             user-select: none;
-
             pointer-events: auto;
+
+            display: flex;
+            flex-direction: row;
 
             /* inner color picker bits */
             --cp-bg-color: rgba(46, 46, 46, 1);
             --cp-border-color: var(--accent-color2);
             --cp-text-color: white;
-            --cp-input-color: rgba(50, 200, 150, 0.3);
-            --cp-button-hover-color: rgba(50, 200, 150, 0.3);
+            --cp-input-color: var(--accent-color);
+            --cp-button-hover-color: var(--accent-color);
 
-            .settings-bloc {
-                color: white;
+						& section {
+                width: 50%;
+						}
+            & .settings-bloc-cont {
+                display: flex;
+                flex-direction: column;
+                gap: 1vw;
+
+								border-right: var(--accent-color2) solid 1px;
+
+								& .settings-bloc {
+                    color: white;
+                }
             }
+
+						& .editor-bloc-cont {
+								padding-left: 1%;
+
+								display: flex;
+								align-items: center;
+								justify-content: center;
+
+								& .editor-button {
+										all: unset;
+
+										cursor: pointer;
+
+										width: 3vw;
+										height: 3vw;
+
+										:global {
+                        svg {
+														width: 100%;
+														height: 100%;
+                        }
+                    }
+                }
+						}
+
+
         }
     }
 
